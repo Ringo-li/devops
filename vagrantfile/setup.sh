be7a51589f15fd6d0d8b62cd61b33a34bded00ee
Ringo <ry.li@qq.com>
2020-12-18 15:07:48 +0800

    secnond

---
+++ vagrantfile/setup.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+sudo yum install -y yum-utils git vim gcc glibc-static telnet bridge-utils net-tools
+sudo yum-config-manager --add-repo  https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker
+sudo systemctl start docker
+sudo usermod -aG dockerroot vagrant
+sudo systemctl restart docker
+docker version
+sudo sed -i /PasswordAuthentication/s/no/yes/ /etc/ssh/sshd_config
+sudo systemctl  restart sshd
