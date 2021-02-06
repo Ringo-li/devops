bf2f304c0d0502300f3e7530853b5e33db7b44db
Ringo <ry.li@qq.com>
2021-02-06 10:03:23 +0800

    new

--- initial/start_vms.sh
+++ initial/start_vms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /d/share/code/devops/Exercise/chapter3/docker-host
+cd /drives/d/share/code/devops/Exercise/chapter3/docker-host
 vagrant up
-cd /d/share/code/devops/Exercise/chapter4
+cd /drives/d/share/code/devops/Exercise/chapter4
 vagrant up
 
